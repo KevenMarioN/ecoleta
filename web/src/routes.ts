a3ab55eaa0533f47ad5a260b78fa447b6ec8334e
KevenMarioN <kevenmario.n.r@gmail.com>
2020-06-05 20:26:00 -0300

    My first commit for ecoleta

---
+++ web/src/routes.ts
@@ -0,0 +1,10 @@
+import { Route,BrowserRouter} from 'react-router-dom';
+import React from 'react';
+
+const Routes = () => {
+    return(
+        <BrowserRouter>
+            
+        </BrowserRouter>
+    );
+}
